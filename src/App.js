c7d81cd7d2bb15a1d0f641e071bccbce63069b85
Emin Durak <emin@pomegra.org>
2018-05-22 21:43:17 +0200

    very simple todo app as a boilerplate for react apollo and apollo-link-state apps

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import TodoList from './TodoList'
+
+import logo from './logo.svg';
+import './App.css';
+
+const Header = () => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className="App-title">Apollo Link State Demo</h1>
+  </header>);
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <TodoList />
+  </div>
+);
+
+export default App;
